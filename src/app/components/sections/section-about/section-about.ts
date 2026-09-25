6ca9b1116535372465b0e7760003bea8f08aeb98
Sven Primus <svenprimus@gmail.com>
2026-09-25 22:53:53 +0200

    added section-about

--- src/app/components/sections/section-about/section-about.ts
+++ src/app/components/sections/section-about/section-about.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { Button } from '../../elements/button/button';
 
 @Component({
-  imports: [],
+  imports: [Button],
   selector: 'app-section-about',
   styleUrl: './section-about.scss',
   templateUrl: './section-about.html',
