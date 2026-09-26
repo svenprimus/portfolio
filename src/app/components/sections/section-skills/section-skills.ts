8a752c0164f07b00e726c305fd1379cceda8972a
Sven Primus <svenprimus@gmail.com>
2026-09-26 19:19:20 +0200

    refined skills animation and text

--- src/app/components/sections/section-skills/section-skills.ts
+++ src/app/components/sections/section-skills/section-skills.ts
@@ -9,6 +9,14 @@ import { Component, signal } from '@angular/core';
 export class SectionSkills {
     visibleLayerNow = signal<number>(3);
 
+    togglePeel() {
+        if (this.visibleLayerNow() == 3) {
+            this.peelOff();
+        } else {
+            this.stickOn();
+        }
+    }
+
     peelOff() {
         const first = setTimeout(() => {
             this.visibleLayerNow.set(2);
@@ -17,6 +25,18 @@ export class SectionSkills {
 
         const second = setTimeout(() => {
             this.visibleLayerNow.set(1);
+            clearTimeout(second);
+        }, 200);
+    }
+
+    stickOn() {
+        const first = setTimeout(() => {
+            this.visibleLayerNow.set(2);
+            clearTimeout(first);
+        }, 100);
+
+        const second = setTimeout(() => {
+            this.visibleLayerNow.set(3);
             clearTimeout(second);
         }, 200);
     }
