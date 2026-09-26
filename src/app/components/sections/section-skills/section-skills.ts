c3e4f38989afd197c15832afc6e7daab09ae0730
Sven Primus <svenprimus@gmail.com>
2026-09-26 05:55:42 +0200

    added skills section

--- src/app/components/sections/section-skills/section-skills.ts
+++ src/app/components/sections/section-skills/section-skills.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
-  imports: [],
-  selector: 'app-section-skills',
-  styleUrl: './section-skills.scss',
-  templateUrl: './section-skills.html',
+    imports: [],
+    selector: 'app-section-skills',
+    styleUrl: './section-skills.scss',
+    templateUrl: './section-skills.html',
 })
-export class SectionSkills {}
+export class SectionSkills {
+    visibleLayerNow = signal<number>(3);
+
+    peelOff() {
+        const first = setTimeout(() => {
+            this.visibleLayerNow.set(2);
+            clearTimeout(first);
+        }, 100);
+
+        const second = setTimeout(() => {
+            this.visibleLayerNow.set(1);
+            clearTimeout(second);
+        }, 200);
+    }
+}
